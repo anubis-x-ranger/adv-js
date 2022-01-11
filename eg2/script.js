f873f3fbea5d87df1db0056ca1e3f8b9d2355605
Satyajit Sahu <anubis-x-ranger@users.noreply.github.com>
2022-01-11 16:23:41 +0530

    update

---
+++ eg2/script.js
@@ -0,0 +1,178 @@
+function showData(choice) {
+  document.getElementById("info").innerHTML = "";
+  if (choice == "object") {
+    let li = document.createElement("li");
+    li.textContent = "Object is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  } else if (choice == "arrays") {
+    let li = document.createElement("li");
+    li.textContent = "It is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  } else if (choice == "functions") {
+    let li = document.createElement("li");
+    li.textContent = "It is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  } else if (choice == "arrays") {
+    let li = document.createElement("li");
+    li.textContent = "It is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  } else if (choice == "strings") {
+    let li = document.createElement("li");
+    li.textContent = "It is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  } else if (choice == "numbers") {
+    let li = document.createElement("li");
+    li.textContent = "It is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  } else if (choice == "null") {
+    let li = document.createElement("li");
+    li.textContent = "It is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  } else if (choice == "undefined") {
+    let li = document.createElement("li");
+    li.textContent = "It is a Non-primitive datatype";
+    var info = document.querySelector("#info");
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent = "All JavaScript values, except primitives, are objects ";
+    info.appendChild(li);
+
+    li = document.createElement("li");
+    li.textContent =
+      "The named values, in JavaScript objects, are called properties.";
+    info.appendChild(li);
+  }
+}
